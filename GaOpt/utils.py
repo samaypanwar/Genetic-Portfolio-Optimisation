72bd6cc0fbc2be0a2a6bbf18f9a60b99a136c200
samaypanwar <69972858+samaypanwar@users.noreply.github.com>
2021-07-25 17:12:31 +0800

    Added hybrid function to optimize

--- GaOpt/utils.py
+++ GaOpt/utils.py
@@ -1,24 +1,36 @@
 # DEF FITNESS FUNCTION HERE
 from quant_risk.statistics import financial_ratios
+from quant_risk.statistics import statistics
 import numpy as np
+import pandas as pd
 
 def sharpe(dataframe, weights):
 
-    portfolio = np.dot(dataframe, weights)
+    portfolio = pd.DataFrame(np.dot(dataframe, weights), index=dataframe.index)
     statistic = financial_ratios.sharpe_ratio(portfolio)
 
-    return statistic
+    return statistic[0]
 
 def calmar(dataframe, weights):
 
-    portfolio = np.dot(dataframe, weights)
+    portfolio = pd.DataFrame(np.dot(dataframe, weights), index=dataframe.index)
     statistic = financial_ratios.calmar_ratio(portfolio)
 
-    return statistic
+    return statistic[0]
 
 def sortino(dataframe, weights):
 
-    portfolio = np.dot(dataframe, weights)
+    portfolio = pd.DataFrame(np.dot(dataframe, weights), index=dataframe.index)
     statistic = financial_ratios.sortino_ratio(portfolio)
+
+    return statistic[0]
+
+def hybrid(dataframe, weights):
+
+    portfolio = pd.DataFrame(np.dot(dataframe, weights), index=dataframe.index)
+    sortino_ratio = financial_ratios.sortino_ratio(portfolio)
+    max_drawdown = statistics.maximum_drawdown(portfolio)
+
+    statistic = 0.4 * sortino_ratio[0] - 0.6 * max_drawdown[0]
 
     return statistic
